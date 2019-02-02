db0988c863bcfec9da926caeca97b8920811ed59
alison.mcdowell <alison.mcdowell@uk.ibm.com>
2019-02-02 23:24:42 +0000

    build.gradle update

--- src/main/java/src/util/ParseInt.java
+++ src/main/java/src/util/ParseInt.java
@@ -3,7 +3,12 @@ package src.util;
 public class ParseInt {
 
 	public int parseInt(String num) {
-		int x = Integer.parseInt(num);
+		int x;
+		try {
+			x = Integer.parseInt(num);
+		} catch (Exception e) {
+			x = -1;
+		}
 		return x;
 	}
 
